1082a130ed670934ecd91898525af0008d5d7731
Suraj Jaiswal <suraj.jaiswal@docquity.com>
2023-02-17 16:54:12 +0530

    Notified

--- Notification/ViewController.swift
+++ Notification/ViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // step 1:
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert,.sound], completionHandler: {(granted,error) in
+             
+        })
+        
+        // step 2: Ceate notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Notification Header"
+        content.body = "Notification Body"
+        
+        // step 3: Create notification trigger
+        let date = Date().addingTimeInterval(10)
+        let dateComponent = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger =  UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
+        
+        // step 4: Create notification request
+        let uuidString = UUID().uuidString
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        // step 5: Register request
+        center.add(request)
     }
 
 
